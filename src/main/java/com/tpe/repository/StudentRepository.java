8b08f64b3e60e4eaa35457206a1ba73f39c22a0f
Jubaw <kaliferno@gmail.com>
2024-05-03 23:32:07 +0300

    second day

--- src/main/java/com/tpe/repository/StudentRepository.java
+++ src/main/java/com/tpe/repository/StudentRepository.java
@@ -5,11 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 //JpaRepo'daki hazır metodları kullanabilmek için extend ediyoruz.
-@Repository
-public interface StudentRepository extends JpaRepository<Student, Long> {
-
-
-
+@Repository //Optional,  to make it more clean and readable
+public interface StudentRepository extends JpaRepository<Student, Long> { //Name of entity class, PK Data Type
+    boolean existsByEmail(String email);
 
 
 
--- src/main/java/com/tpe/service/StudentService.java
+++ src/main/java/com/tpe/service/StudentService.java
@@ -1,9 +1,14 @@
 package com.tpe.service;
 
 
+import com.tpe.domain.Student;
+import com.tpe.exceptions.ConflictException;
 import com.tpe.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Service
 public class StudentService {
@@ -12,5 +17,22 @@ public class StudentService {
     private StudentRepository studentRepository;
 
 
+    //getAll()*******************************************************
+    public List<Student> getAll() {
+        return studentRepository.findAll();
+        //Don't need to control anything, getAll has no handle cases
+    }
+
+
+    public void createStudent(Student student) {
+        //Check if email is unique
+
+        if (studentRepository.existsByEmail(student.getEmail())){
+            throw new ConflictException("Email already exists");
+        }
+
+        studentRepository.save(student);
+
+    }
 
 }
